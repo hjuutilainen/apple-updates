dd120f08bc66c4ff0064a2a4b44d4fd732f0d946
Hannes Juutilainen <hjuutilainen@mac.com>
2021-06-09 08:40:01 +0300

    Update postprocess-catalog-gz.js

--- postprocessing/postprocess-catalog-gz.js
+++ postprocessing/postprocess-catalog-gz.js
@@ -1,6 +1,6 @@
 import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.10/mod.ts'
 import { gzipFile, gunzipFile } from 'https://deno.land/x/compress@v0.3.8/gzip/mod.ts'
-import * as path from 'https://deno.land/std/path/mod.ts'
+import * as path from 'https://deno.land/std@0.97.0/path/mod.ts'
 
 const filename = Deno.args[0]
 console.log(filename)
